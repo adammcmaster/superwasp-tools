610e3bd0db466a6fbb2af9ed82c1a97ad0505634
Adam McMaster <adam.mcmaster@open.ac.uk>
2020-12-01 17:06:57 +0000

    Add manual real vs junk classifications

--- swasputils.py
+++ swasputils.py
@@ -68,6 +68,11 @@ def load_classifications():
     ], 'columns', inplace=True)
     return classifications
 
+def load_manual_classifications():
+    return pandas.read_csv(
+        os.path.join(DATA_LOCATION, 'superwasp-long-periods-classifications.csv'),
+    )[['subject_ids', 'annotations']]
+
 def merge_zoo_ids(objects, lookup):
     MERGE_FIELDS = ['SWASP ID', 'Period Number']
     objects['SWASP ID'] = objects[['SWASP', 'ID']].apply(
@@ -94,8 +99,23 @@ def merge_classifications(objects, classifications):
         how='left',
     )
 
+def merge_manual_classifications(objects, classifications):
+    return pandas.merge(
+        objects,
+        classifications,
+        left_on='Zooniverse ID',
+        right_on='subject_ids',
+        how='left',
+    ).drop('subject_ids', 'columns')
+
 def decode_zoo_locations(objects):
     objects['Lightcurve'] = objects['locations'].apply(
         lambda s: yaml.load(s)['0']
     )
     return objects.drop('locations', 'columns')
+
+def decode_manual_annotations(objects):
+    objects['Manual Classification'] = objects['annotations'].apply(
+        lambda s: yaml.load(s)[0]['value'] if s else ''
+    )
+    return objects.drop('annotations', 'columns')
